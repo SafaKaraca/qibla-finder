c2dae5ac075966907f6995232111995c6a2f0e5f
SafaKaraca <karacamuhammedsafa@gmail.com>
2024-09-09 10:09:16 +0300

    feat: Splash screen is added.

--- App.js
+++ App.js
@@ -1,6 +1,20 @@
 import React from 'react';
-import QiblaScreen from './src/screens/QiblaScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SplashScreen from './src/screens/SplashScreen.js'; // SplashScreen yolunu doğru belirt
+import QiblaScreen from './src/screens/QiblaScreen.js';  // QiblaScreen yolunu doğru belirt
 
-export default function App() {
-  return <QiblaScreen />;
-}
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SplashScreen" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="SplashScreen" component={SplashScreen} />
+        <Stack.Screen name="QiblaScreen" component={QiblaScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
